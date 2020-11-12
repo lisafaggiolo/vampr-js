720844590de42917e6e62e4495e9361b63d56f44
lisa <lisa.faggiolo@live.ca>
2020-11-12 18:22:10 -0500

    added Methods to Vampire Class

--- vampire.js
+++ vampire.js
@@ -5,28 +5,49 @@ class Vampire {
     this.offspring = [];
     this.creator = null;
   }
-
-  /** Simple tree methods **/
-
-  // Adds the vampire as an offspring of this vampire
-  addOffspring(vampire) {
-
-  }
-
-  // Returns the total number of vampires created by that vampire
-  get numberOfOffspring() {
-
-  }
-
-  // Returns the number of vampires away from the original vampire this vampire is
-  get numberOfVampiresFromOriginal() {
-
-  }
-
-  // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
-  isMoreSeniorThan(vampire) {
-
-  }
+  
+    /** Simple tree methods **/
+  
+    // Adds the vampire as an offspring of this vampire
+    addOffspring(vampire) {
+      this.offspring.push(vampire)
+      vampire.creator = this;
+    }
+  
+    // Returns the total number of vampires created by that vampire
+    get numberOfOffspring() {
+      return this.offspring.length
+    }
+  
+    // Returns the number of vampires away from the original vampire this vampire is
+    get numberOfVampiresFromOriginal() {
+      let numOfAncestors = 0;
+      let descendant = this;
+      
+      if (descendant.creator === null){
+        return numOfAncestors;
+      } else {
+        while (descendant.creator) {
+          descendant = descendant.creator;
+          numOfAncestors++
+        }
+      }
+      return numOfAncestors;
+    }
+  
+    // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
+    isMoreSeniorThan(vampire) {
+  
+      let vampireAncestors = vampire.numberOfVampiresFromOriginal
+      let thisAncestors = this.numberOfVampiresFromOriginal
+      console.log('vampireAncestor =>', vampireAncestors);
+      console.log('thisAncestors =>', thisAncestors);
+      if (thisAncestors < vampireAncestors) {
+        return true;
+      } else {
+        return false;
+      }
+    }
 
   /** Stretch **/
 
